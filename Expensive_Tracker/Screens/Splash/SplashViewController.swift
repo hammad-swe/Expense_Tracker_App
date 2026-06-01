9c3eea7234cafda9d2a4f6280f97cfa9788e2bf8
Hammad Ali <hammadali78056@gmail.com>
2026-06-01 16:47:28 +0500

    Initial Commit

---
+++ Expensive_Tracker/Screens/Splash/SplashViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SplashViewController.swift
+//  Expensive_Tracker
+//
+//  Created by Hammad Ali on 01/06/2026.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
---
+++ Expensive_Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Expensive_Tracker
+//
+//  Created by Hammad Ali on 01/06/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
